1993a9dc27c5fc9100437f131e69daea116bb7ad
Jaroslaw Palka <jaroslaw.palka@allegrogroup.com>
2016-08-25 16:36:27 +0200

    experiments with grpc as a protocol for autocomplete backend

--- backend/autocomplete-java-server/src/main/java/io/atom/autocomplete/java/Bootstrap.java
+++ backend/autocomplete-java-server/src/main/java/io/atom/autocomplete/java/Bootstrap.java
@@ -5,6 +5,9 @@ import java.io.IOException;
 import io.grpc.Server;
 import io.grpc.ServerBuilder;
 import io.grpc.examples.helloworld.GreeterGrpc;
+import io.grpc.examples.helloworld.HelloReply;
+import io.grpc.examples.helloworld.HelloRequest;
+import io.grpc.stub.StreamObserver;
 
 public class Bootstrap {
 
@@ -12,6 +15,13 @@ public class Bootstrap {
 		Server server = ServerBuilder
 		.forPort(8080)
 		.addService(new GreeterGrpc.GreeterImplBase() {
+
+            @Override
+            public void sayHello(HelloRequest request, StreamObserver<HelloReply> responseObserver) {
+                responseObserver.onNext(HelloReply.newBuilder().setMessage("Hello "+request.getName()).build());
+                responseObserver.onCompleted();
+            }
+			
 		})
 		.build()
 		.start();
